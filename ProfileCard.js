31aceaa538690fbe9ba45fb53634222e3628985f
wstri <wstriii@outlook.com>
2025-05-30 20:18:14 -0700

    Code2

--- ProfileCard.js
+++
@@ -1,9 +0,0 @@
-export default function ProfileCard({ user }) {
-  return (
-    <div className="p-4 border rounded-xl shadow-lg bg-white/10">
-      <img src={user.avatar} alt="PFP" className="rounded-full w-20 h-20" />
-      <h2>{user.displayName}</h2>
-      <p>{user.bio}</p>
-    </div>
-  );
-}
